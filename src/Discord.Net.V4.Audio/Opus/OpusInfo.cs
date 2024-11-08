5354e854c90976d2402921408462022327e21466
SineVector241 <69619913+SineVector241@users.noreply.github.com>
2024-11-08 18:54:22 +1100

    Add opusInfo. Converted all DllImport Attributes to use LibraryImport attribute instead for AOT compatibility. Needed to also convert net6.0 to net7.0 to support LibraryImport attribute.

---
+++ src/Discord.Net.V4.Audio/Opus/OpusInfo.cs
@@ -0,0 +1,31 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Discord.Audio.Opus
+{
+    /// <summary>
+    /// Provides information about the opus DLL.
+    /// </summary>
+    public class OpusInfo
+    {
+        /// <summary>
+        /// Gets the libopus version string.
+        /// </summary>
+        /// <returns>Version string.</returns>
+        public static unsafe string Version()
+        {
+            byte* version = NativeOpus.opus_get_version_string();
+            return Marshal.PtrToStringAnsi((IntPtr)version) ?? "";
+        }
+
+        /// <summary>
+        /// Converts an opus error code into a human readable string.
+        /// </summary>
+        /// <param name="error">Error number.</param>
+        /// <returns>Error string.</returns>
+        public static unsafe string StringError(int error)
+        {
+            byte* stringError = NativeOpus.opus_strerror(error);
+            return Marshal.PtrToStringAnsi((IntPtr)stringError) ?? "";
+        }
+    }
+}
